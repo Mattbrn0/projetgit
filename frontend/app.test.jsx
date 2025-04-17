14f04d866c62d704e1f2bc622a34410390aac6d2
Charon <charon.s.tom@gmail.com>
2025-04-17 13:29:18 +0200

    implementing test in CI

--- frontend/app.test.jsx
+++ frontend/app.test.jsx
@@ -4,7 +4,6 @@ import { add } from "../frontend/src/App";
 
 describe("add function", () => {
   it("adds two numbers correctly", () => {
-    expect(add(2, 3)).toBe(51);
-    expect(add(-1, 1)).toBe(9);
+    expect(add(2, 3)).toBe(5);
   });
 });
