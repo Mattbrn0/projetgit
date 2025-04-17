b270c62379107277ebccafa3ad5f9ef6300fa5f5
Charon <charon.s.tom@gmail.com>
2025-04-17 16:10:46 +0200

    feat(api):get data from the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,46 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import MovieRow from "./components/MovieRow";
 import Footer from "./components/Footer";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/movie`);
+        console.log("Réponse axios :", response.data);
+        setResult(response.data);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-black text-white min-h-screen">
       <Navbar />
-      <Hero />
       <section className="space-y-8 px-4 md:px-8 mt-8">
-        <MovieRow title="Trending Now" />
+        {result ? (
+          result.map((movie, index) => (
+            <div key={index} className="border-b border-gray-700 pb-2">
+              {movie.title}
+            </div>
+          ))
+        ) : (
+          <div>Chargement...</div>
+        )}
+
+        {/* <MovieRow title="Trending Now" />
         <MovieRow title="Top Rated" />
         <MovieRow title="New Releases" />
         <MovieRow title="Action Movies" />
-        <MovieRow title="TV Shows" />
+        <MovieRow title="TV Shows" /> */}
       </section>
       <Footer />
     </div>
